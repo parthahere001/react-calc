5aeb59cd2551b79b0860f100ac24568f9376e46a
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2023-01-03 18:50:59 +0530

    Coded all the different components of the calculator

---
+++ src/components/calculator.js
@@ -0,0 +1,13 @@
+// Import React
+import React from 'react';
+  
+// Create a Class Component Calculator
+class Calculator extends React.Component {
+    render()
+    {
+        return "React-Calc";
+    }
+}
+  
+// Export Calculator.
+export default Calculator;
